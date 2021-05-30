9f997426425fb05c252fa6109a9abdeddf6a762d
David Luiz <dvdluiz1991@gmail.com>
2021-05-30 10:12:28 -0300

    curso online js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 62.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //Condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Digija sempre usando cinto de segurança!`)
